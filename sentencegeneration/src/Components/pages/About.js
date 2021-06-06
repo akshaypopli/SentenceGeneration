b70b56021aa4c08e8efda1aa7b6135331dee23d1
Mac <mac@Akshays-Air.local>
2021-06-05 23:33:43 -0400

    updated the about page

--- sentencegeneration/src/Components/pages/About.js
+++ sentencegeneration/src/Components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <Fragment>
             <h1>About this app</h1>
-            <p>App to search Github users</p>
+            <p>App to Make a Sentence using a sentence generating API</p>
             <p>Version 1.0.0</p>
         </Fragment>
     )
